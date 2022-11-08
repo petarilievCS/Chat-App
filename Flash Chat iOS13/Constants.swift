9f13d566879b8bd87348dd00bb941e7da5ff9f04
Petar Iliev <petariliev2002@gmail.com>
2022-11-08 08:37:37 -0600

    Implemented logging out users with Firebase

---
+++ Flash Chat iOS13/Constants.swift
@@ -0,0 +1,13 @@
+//
+//  Constants.swift
+//  Flash Chat iOS13
+//
+//  Created by Petar Iliev on 8.11.22.
+//  Copyright © 2022 Angela Yu. All rights reserved.
+//
+
+struct Constants {
+    let registerSegue = "registerToChat"
+    let loginSegue = "loginToChat"
+    let title = "⚡️FlashChat"
+}
--- Flash Chat iOS13/Controllers/ChatViewController.swift
+++ Flash Chat iOS13/Controllers/ChatViewController.swift
@@ -8,6 +8,7 @@
 
 
 import UIKit
+import FirebaseAuth
 
 class ChatViewController: UIViewController {
 
@@ -16,11 +17,20 @@ class ChatViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        title = "⚡️FlashChat"
+        navigationItem.hidesBackButton = true
     }
     
     @IBAction func sendPressed(_ sender: UIButton) {
+        
     }
     
-
+    @IBAction func logoutButtonPressed(_ sender: UIBarButtonItem) {
+        do {
+            try Auth.auth().signOut()
+            navigationController?.popToRootViewController(animated: true)
+        } catch let signOutError as NSError {
+            print("Error signing out: %@", signOutError)
+        }
+    }
 }
